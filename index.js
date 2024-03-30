9d787e76fd10da791ff40247992213cf570fa5b4
MuhammadAnas02 <161252236+MuhammadAnas02@users.noreply.github.com>
2024-03-30 13:02:50 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let anas = await inquirer.prompt([{
+            name: "todo",
+            type: "input",
+            message: "what you want to add in your Todos",
+        },
+        {
+            name: "check",
+            type: "confirm",
+            message: "Are you sure",
+            default: false,
+        },
+        {
+            name: "confirmation",
+            type: "list",
+            message: "Do want add more?",
+            choices: ["Add", "Remove"],
+        },
+    ]);
+    if (anas.confirmation === "Add") {
+        todos.push(anas.todo);
+        condition = anas.check;
+        console.log(todos);
+    }
+    else if (anas.confirmation === "Remove") {
+        todos.pop();
+        condition = anas.check;
+        console.log(todos);
+    }
+}
